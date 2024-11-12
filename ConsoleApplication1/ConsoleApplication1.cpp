94c6a17c76726810c38a50c4a6b0fd254d03f068
dengkai1001 <4B3G0106@stust.edu.tw>
2024-11-12 11:38:21 +0800

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,4 @@
-﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 //
 
 #include <iostream>
@@ -6,5 +6,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	int n = 1;
+	int sum = 0;
+	cout << "請輸入一個正整數n";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 == 0)sum += i;
+	}
+	cout << "1~" << n << "之間所有偶數的和為:" << sum << endl;
 }
