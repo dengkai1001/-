c349a1fccb19f1189cad117070ac1533ee0d5722
dengkai1001 <4B3G0106@stust.edu.tw>
2024-11-12 11:37:35 +0800

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,4 @@
-﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 //
 
 #include <iostream>
@@ -6,5 +6,36 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	int month = 1;
+	string result = "";
+	cout << "請輸入月份(1-12):";
+	cin >> month;
+
+	if (month < 1 or month > 12) {
+		cout << "請輸入有效月份" << endl;
+	}
+	else {
+		switch (month) {
+		case 3:
+		case 4:
+		case 5:
+			result = "春季";
+			break;
+		case 6:
+		case 7:
+		case 8:
+			result = "夏季";
+			break;
+		case 9:
+		case 10:
+		case 11:
+			result = "秋季";
+			break;
+		default:
+			result = "冬季";
+			break;
+		}
+		cout << month << "月是" << result << endl;
+	
 }
+	
